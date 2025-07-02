fecbd4b36bccd33e1c19fa3d45d43714c60fc1a2
Shakila shaik <skshakilask67@gmail.com>
2025-07-02 11:53:21 +0530

    again initiating project

--- auth-service/app/routes/auth.py
+++
@@ -1,8 +0,0 @@
-# app/routes/auth.py
-from flask import Blueprint, request, jsonify
-
-auth_bp = Blueprint('auth', __name__)
-
-@auth_bp.route('/register', methods=['POST'])
-def register():
-    return jsonify({"message": "Register works!"}), 200
