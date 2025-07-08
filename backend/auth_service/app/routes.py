6f134cd5476afed4aa26aab3eed7a5331d4192f0
Shakila shaik <skshakilask67@gmail.com>
2025-07-08 16:25:13 +0530

    completed login api and verify password , access token generation

--- backend/auth_service/app/routes.py
+++ backend/auth_service/app/routes.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.exc import IntegrityError
 from app.dbconnect import async_session
-from app.schemas import UserRegister
+from app.schemas import UserRegister, UserLogin
 from app.models import User
-from app.utils import hash_password
+from app.utils import hash_password, verify_password, create_access_token
 from sqlalchemy import select
 
 
@@ -46,3 +46,26 @@ async def register(user: UserRegister, db: async_session = Depends(get_db)):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Failed to create user.",
         )
+
+
+@router.post("/login")
+async def login(user: UserLogin, db: async_session = Depends(get_db)):
+    stmt = select(User).where(User.email == user.email)
+    result = await db.execute(stmt)
+    db_user = result.scalar_one_or_none()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found , please register",
+        )
+    if not verify_password(user.password, db_user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=" incorrect password"
+        )
+    token = create_access_token({"user_id": db_user.id})
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        "message": "Login successful",
+    }
