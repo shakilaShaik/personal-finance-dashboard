b68eb544784b98300d18b712a54d83b48d0279bd
Shakila shaik <skshakilask67@gmail.com>
2025-07-19 15:44:06 +0530

    added auth as header

--- backend/auth_service/app/deps.py
+++ backend/auth_service/app/deps.py
@@ -1,23 +1,18 @@
 # app/api/deps.py
-
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException
+from fastapi.security import APIKeyHeader
 from app.utils import decode_token
 
-oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="/login"
-)  # Make sure this route returns token
+api_key_header = APIKeyHeader(name="Authorization")
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(api_key_header)):
+    if not token.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Invalid token format")
+
+    token = token.split("Bearer ")[1]
     payload = decode_token(token)
     if not payload:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token"
-        )
-    user_id: str = payload.get("user_id")
-    if user_id is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token payload"
-        )
-    return {"user_id": user_id}
+        raise HTTPException(status_code=401, detail="Token is invalid or expired")
+
+    return {"user_id": payload.get("user_id")}
